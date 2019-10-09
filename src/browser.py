745ae0c73bdc7fbaee82315a61a1f2a85666d312
paul-hyun <cchyun@gmail.com>
2019-10-09 16:18:34 +0900

    목록 상세 버그 수정

--- src/browser.py
+++ src/browser.py
@@ -14,12 +14,12 @@ def make_list_item(data, id, start=0):
     text = ""
     for item in data[start:start+5]:
         if str(item.id) == id:
-            aaa = {'text': f'<b>[ {data[0].CODENAME} ] [ {data[0].TITLE} ]</b>' + '<br>' +
-                    f'날짜 : {data[0].DATE}' + '<br>' +
-                    f'장소 : {data[0].ORG_NAME} {data[0].PLACE}' + '<br>' +
-                    f'연령 : {data[0].USE_TRGT}' + '<br>' +
-                    f'요금 : {data[0].USE_FEE}' + '<br>' + '<br>' +
-                    f'<a href="{data[0].ORG_LINK}" target = "_blank"><img src="{data[0].MAIN_IMG}" style="max-height: 360px; max-width: 360px; object-fit: contain" /></a>'
+            aaa = {'text': f'<b>[ {item.CODENAME} ] [ {item.TITLE} ]</b>' + '<br>' +
+                    f'날짜 : {item.DATE}' + '<br>' +
+                    f'장소 : {item.ORG_NAME} {item.PLACE}' + '<br>' +
+                    f'연령 : {item.USE_TRGT}' + '<br>' +
+                    f'요금 : {item.USE_FEE}' + '<br>' + '<br>' +
+                    f'<a href="{item.ORG_LINK}" target = "_blank"><img src="{item.MAIN_IMG}" style="max-height: 360px; max-width: 360px; object-fit: contain" /></a>'
             }
             text += aaa["text"]
         else:
