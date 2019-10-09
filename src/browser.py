c28e2cef3f91eb0982f3d9bcbdd365803982ee68
ubuntu <ubuntu@hellonms.net>
2019-10-09 16:30:29 +0000

    출력포멧 변경

--- src/browser.py
+++ src/browser.py
@@ -20,12 +20,12 @@ def make_list_item(data, id, start=0):
     text = ""
     for item in data[start:start+5]:
         if str(item.id) == id:
-            aaa = {'text': f'<b>[ {item.CODENAME} ] [ {item.TITLE} ]</b>' + '<br>' +
+            aaa = {'text': f'<p><b>[ {item.CODENAME} ] [ {item.TITLE} ]</b>' + '<br>' +
                     f'날짜 : {item.DATE}' + '<br>' +
                     f'장소 : {item.ORG_NAME} {item.PLACE}' + '<br>' +
                     f'연령 : {item.USE_TRGT}' + '<br>' +
                     f'요금 : {item.USE_FEE}' + '<br>' + '<br>' +
-                    f'<a href="{item.ORG_LINK}" target = "_blank"><img src="{item.MAIN_IMG}" style="max-height: 360px; max-width: 360px; object-fit: contain" /></a>' + '<br>'
+                    f'<a href="{item.ORG_LINK}" target = "_blank"><img src="{item.MAIN_IMG}" style="max-height: 360px; max-width: 360px; object-fit: contain" /></a></p>'
             }
             text += aaa["text"]
         else:
@@ -40,12 +40,12 @@ def make_list_item(data, id, start=0):
 
 
 def make_detail(data):
-    text = {'text': f'<b>[ {data[0].CODENAME} ] [ {data[0].TITLE} ]</b>' + '<br>' +
+    text = {'text': f'<p><b>[ {data[0].CODENAME} ] [ {data[0].TITLE} ]</b>' + '<br>' +
                     f'날짜 : {data[0].DATE}' + '<br>' +
                     f'장소 : {data[0].ORG_NAME} {data[0].PLACE}' + '<br>' +
                     f'연령 : {data[0].USE_TRGT}' + '<br>' +
                     f'요금 : {data[0].USE_FEE}' + '<br>' + '<br>' +
-                    f'<a href="{data[0].ORG_LINK}" target = "_blank"><img src="{data[0].MAIN_IMG}" style="max-height: 360px; max-width: 360px; object-fit: contain" /></a>'
+                    f'<a href="{data[0].ORG_LINK}" target = "_blank"><img src="{data[0].MAIN_IMG}" style="max-height: 360px; max-width: 360px; object-fit: contain" /></a></p>'
             }
     return text['text']
 
