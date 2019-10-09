67026b09cc307a82deb9c2fbf93d755f1ee16af6
paul-hyun <cchyun@gmail.com>
2019-10-09 04:11:30 +0000

    외부 사용자 추가

--- doc/env.sql
+++ doc/env.sql
@@ -1,12 +1,18 @@
 -- DB 생성
 CREATE DATABASE tourbot;
 
+
 -- 계정생성
 CREATE USER 'tourbot'@'localhost' IDENTIFIED BY 'tourbot123!';
+CREATE USER 'tourbot'@'%' IDENTIFIED BY 'tourbot123!';
+
 
 -- 권한
 GRANT ALL PRIVILEGES ON tourbot.* TO 'tourbot'@'localhost';
 SHOW GRANTS FOR 'tourbot'@'localhost';
+GRANT ALL PRIVILEGES ON tourbot.* TO 'tourbot'@'%';
+SHOW GRANTS FOR 'tourbot'@'%';
+
 
 -- 권한 적용
 FLUSH PRIVILEGES;
